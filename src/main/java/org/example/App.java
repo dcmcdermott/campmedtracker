da55099a689b58a4b92f89e05797562e175a1386
Devin McDermott <dc.mcdermott22@gmail.com>
2021-04-23 11:27:57 -0400

    Icon
    App window icon and title set
    
    Devin McDermott <dc.mcdermott22@gmail.com>

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,6 +10,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -24,6 +25,8 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("camper_table"), 1200, 700);
+        stage.getIcons().add(new Image("https://cdn.iconscout.com/icon/free/png-512/drugs-26-129384.png"));
+        stage.setTitle("Adventure Camp Med Tracker");
         stage.setScene(scene);
         stage.show();
     }
