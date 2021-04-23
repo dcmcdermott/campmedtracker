75d624224ca245ca5602dedfd46d6ad7c086944b
Devin McDermott <dc.mcdermott22@gmail.com>
2021-04-23 00:48:05 -0400

    Current camper name added to add_prescription
    Devin McDermott <dc.mcdermott22@gmail.com>

--- src/main/java/org/example/ControllerAddPrescriptions.java
+++ src/main/java/org/example/ControllerAddPrescriptions.java
@@ -1,30 +1,62 @@
 package org.example;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.net.URL;
+import java.sql.*;
+import java.util.ResourceBundle;
 
-public class ControllerAddPrescriptions {
+public class ControllerAddPrescriptions implements Initializable {
+
     public Label lblCamperName;
     public TextField tfMedName;
     public TextField tfDose;
     public TextField tfAdminTime;
-    public Button btnFinish;
+    public Button btnAdd;
     public Button btnCancel;
-
-    //ControllerAddCamper controllerAddCamper = new ControllerAddCamper();
     public String camperID = ControllerAddCamper.camperID;
 
     @FXML
+    public void initialize(URL location, ResourceBundle resources) {
+        try (Connection con = DBDriver.getConnection()) {
+
+            // mysql statement
+            String query = "select first_name, last_name from campers where camper_id = ?";
+
+            PreparedStatement preparedStmt = con.prepareStatement(query);
+            preparedStmt.setString (1, camperID);
+
+            ResultSet rs = preparedStmt.executeQuery();
+
+            String lname = "";
+            String fname = "";
+
+            while (rs.next()) {
+                lname = rs.getString("last_name");
+                fname = rs.getString("first_name");
+            }
+
+            lblCamperName.setText(fname + " " + lname);
+        }
+        catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+
+        @FXML
     private void switchToAddCamper() throws IOException {
         App.setRoot("add_camper");
+    }
+
+    @FXML
+    private void switchToCampers() throws IOException {
+        App.setRoot("camper_table");
     }
 
     @FXML
@@ -36,14 +68,14 @@ public class ControllerAddPrescriptions {
             String query = " insert into prescriptions (medname, dose, admintime, userid)"
                     + " values (?, ?, ?, ?)";
 
-            // create the mysql preparedstatement
+            // create the mysql prepared statement
             PreparedStatement preparedStmt = con.prepareStatement(query);
             preparedStmt.setString (1, tfMedName.getText());
             preparedStmt.setString (2, tfDose.getText());
             preparedStmt.setString (3, tfAdminTime.getText());
             preparedStmt.setString (4, camperID);
 
-            // execute the preparedstatement
+            // execute the prepared statement
             preparedStmt.execute();
 
             System.out.println("Prescriptions added successfully");
